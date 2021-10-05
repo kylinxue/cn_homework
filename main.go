1da6166494150f6f695c6eedcf406d16bf36d8fa
kylinxue <keylinxue@yeah.net>
2021-10-05 14:19:19 +0800

    http server

---
+++ main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	"io"
+	"log"
+	"net/http"
+
+	"github.com/golang/glog"
+)
+
+func main() {
+	_ = flag.Set("v", "4")
+	flag.Parse()
+	glog.V(2).Info("Starting http server...")
+
+	http.HandleFunc("/headers", echoHeaders)
+	http.HandleFunc("/infos", infos)
+	http.HandleFunc("/healthz", healthz)
+
+	err := http.ListenAndServe(":80", nil)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func infos(w http.ResponseWriter, r *http.Request) {
+
+	env := os.Getenv("JAVA_HOME1")
+	host := r.Host
+	fmt.Println(host, env)
+}
+
+// 返回请求的Headers
+func echoHeaders(w http.ResponseWriter, r *http.Request) {
+	headers := r.Header
+	for k, v := range headers {
+		w.Header().Add(k, v[0])
+	}
+	glog.Info("headers transfer to response!")
+}
+
+// 探活
+func healthz(w http.ResponseWriter, r *http.Request) {
+	_, _ = io.WriteString(w, "ok\n")
+}
